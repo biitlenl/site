47352d944d3287ae91dd71ea2ef2571d2146376a
fgclue <lost@biitle.nl>
2026-01-16 14:20:26 -0300

    Setup custom fonts in Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
+    experimental: {
+        fonts: [{
+            provider: fontProviders.google(),
+            name: 'Slabo 27px',
+            cssVariable: '--font-slabo'
+        }]
+    },
     site: 'https://biitle.nl',
     vite: {
         plugins: [tailwindcss()],
