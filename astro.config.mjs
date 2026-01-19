42a70b4c1f41441ec924e820e5de0c99f4f9a8e3
fgclue <lost@biitle.nl>
2026-01-19 16:53:45 -0300

    Add Roboto to fonts

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,27 @@
 // @ts-check
-import { defineConfig, fontProviders } from 'astro/config';
+import { defineConfig, fontProviders } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import sitemap from '@astrojs/sitemap'
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        fonts: [{
-            provider: fontProviders.google(),
-            name: 'Slabo 27px',
-            cssVariable: '--font-slabo'
-        }]
-    },
-    site: 'https://biitle.nl',
-    vite: {
-        plugins: [tailwindcss()],
-    },
-    integrations: [sitemap()]
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Slabo 27px",
+        cssVariable: "--font-slabo",
+      },
+      {
+        provider: fontProviders.google(),
+        name: "Roboto",
+        cssVariable: "--font-roboto",
+      },
+    ],
+  },
+  site: "https://biitle.nl",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [sitemap()],
 });
