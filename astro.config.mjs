01b066dbc6b860698e2a393798855b24519c2523
fgclue <lost@biitle.nl>
2026-03-29 22:30:43 -0300

    Update astro to v6

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,11 @@ import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-    experimental: {
-        fonts: [{
-            provider: fontProviders.google(),
-            name: 'Slabo 27px',
-            cssVariable: '--font-slabo'
-        }]
-    },
+    fonts: [{
+        provider: fontProviders.google(),
+        name: 'Slabo 27px',
+        cssVariable: '--font-slabo'
+    }],
     site: 'https://biitle.nl',
     vite: {
         plugins: [tailwindcss()],
