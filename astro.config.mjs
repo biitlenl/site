5c7c032fd21e9ff537b3218bee5081f671eaf4bc
fgclue <lost@biitle.nl>
2026-04-23 00:38:40 -0300

    Fix site

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,14 +5,21 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-    fonts: [{
-        provider: fontProviders.google(),
-        name: 'Slabo 27px',
-        cssVariable: '--font-slabo'
-    }],
-    site: 'https://biitle.nl',
-    vite: {
-        plugins: [tailwindcss()],
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Slabo 27px",
+      cssVariable: "--font-slabo",
     },
-    integrations: [sitemap()]
+    {
+      provider: fontProviders.google(),
+      name: "Roboto",
+      cssVariable: "--font-roboto",
+    },
+  ],
+  site: "https://biitle.nl",
+  vite: {
+    plugins: [tailwindcss()],
+  },
+  integrations: [sitemap()],
 });
